b160e832bee97944af22bbff30dc67e88a5d9df5
Randall Tom <randall.tom23@gmail.com>
2017-11-08 00:44:58 -0800

    Final

--- meme2/MemeCollectionViewController.swift
+++ meme2/MemeCollectionViewController.swift
@@ -43,7 +43,6 @@ class MemeCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
         let meme = memes[indexPath.item]
         let imageView = UIImageView(image: meme.memedImage)
-        //imageView.contentMode = .scaleAspectFit
         imageView.contentMode = .scaleAspectFit
         cell.backgroundView = imageView
         return cell
